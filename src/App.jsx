a9ae47d9477581b4a61ace2c8025f806db88de31
Pablo Sangenis <sangenispablo@gmail.com>
2023-09-20 19:16:33 -0300

    Cambio a App el BrowseRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { AppRouter } from "./router/AppRouter";
+import { BrowserRouter } from "react-router-dom";
 
+import { AppRouter } from "./router/AppRouter";
 import { AppTheme } from "./theme";
 
 export const App = () => {
   return (
     <AppTheme>
-      <AppRouter />
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </AppTheme>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
-
 import { App } from "./App";
 
 import "@fontsource/roboto/300.css";
@@ -14,8 +12,6 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
